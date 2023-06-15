f9cace0a7af8f179536b9842eea3b3eccdbb0918
camelStyleUser <124447863+camelStyleUser@users.noreply.github.com>
2023-06-15 10:41:07 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,4 @@
 Items.surgeAlloy.hardness=5;
 Blocks.blastDrill.drillMultipliers.put(Items.surgeAlloy,0.05)
+Blocks.eruptionDrill.drillMultipliers.put(Items.surgeAlloy,0.05)
+Blocks.impactDrill.drillMultipliers.put(Items.surgeAlloy,0.05)
